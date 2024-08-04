acac4d0eacfae290132ddd71610decbd50035855
Alif Rizki Ramdhana <alifrizki1011@gmail.com>
2024-08-04 12:32:23 +0700

    Idle to Walking

--- Foxy Adventure/Assets/Script/FoxyMove.cs
+++ Foxy Adventure/Assets/Script/FoxyMove.cs
@@ -5,21 +5,50 @@ using UnityEngine;
 public class FoxyMove : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+    private float dirX;
+    [SerializeField] private float moveSpeed = 6f;
+    [SerializeField] private float jumpForce = 10f;
+
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirX * 6f, rb.velocity.y);
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 10f);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+        // Assign Animasi
+        if (dirX > 0)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else 
+        {
+            anim.SetBool("running", false);
         }
     }
 }
