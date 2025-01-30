c25f9c30bb3bd3f1ff2c28b491d8da7b4f5f07c4
Alif Rizki Ramdhana <alifrizki1011@gmail.com>
2025-01-31 00:32:33 +0700

    update: New Script Response Handler

--- Foxy Adventure/Assets/Script/Dialogue/DialogueObject.cs
+++ Foxy Adventure/Assets/Script/Dialogue/DialogueObject.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class DialogueObject : ScriptableObject
 {
     [SerializeField] [TextArea] private string[] dialogue;
+    [SerializeField] private Response[] responses;
 
     public string[] Dialogue => dialogue;
+
+    public Response[] Responses => responses;
 }
