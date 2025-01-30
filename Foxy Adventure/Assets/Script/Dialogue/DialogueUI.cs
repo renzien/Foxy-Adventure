a3312a13afd7969438d9a98fdcc1d7079a8fd7a4
Alif Rizki Ramdhana <alifrizki1011@gmail.com>
2025-01-30 22:06:04 +0700

    update: Dialogue Data

--- Foxy Adventure/Assets/Script/Dialogue/DialogueUI.cs
+++ Foxy Adventure/Assets/Script/Dialogue/DialogueUI.cs
@@ -1,12 +1,42 @@
+using System.Collections;
 using UnityEngine;
 using TMPro;
 
 public class DialogueUI : MonoBehaviour
 {
+    [SerializeField] private GameObject dialogueBox;
     [SerializeField] private TMP_Text labelText;
+    [SerializeField] private DialogueObject testDialogue;
+
+    private TypeEffect typeEffect;
 
     private void Start()
     {
-        labelText.text = "Hello! Mr. Froggy\nHow are you doing today?";
+        typeEffect = GetComponent<TypeEffect>();
+        CloseDialogueBox();
+        ShowDialogue(testDialogue);
+    }
+
+    public void ShowDialogue(DialogueObject dialogueObject)
+    {
+        dialogueBox.SetActive(true);
+        StartCoroutine(StepThroughDialogue(dialogueObject));
+    }
+
+    private IEnumerator StepThroughDialogue(DialogueObject dialogueObject)
+    {
+        foreach (string dialogue in dialogueObject.Dialogue)
+        {
+            yield return typeEffect.Run(dialogue, labelText);
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.E));
+        }
+
+        CloseDialogueBox();
+    }
+
+    private void CloseDialogueBox()
+    {
+        dialogueBox.SetActive(false);
+        labelText.text = string.Empty;
     }
 }
--- Foxy Adventure/Assets/Script/Dialogue/TypeEffect.cs
+++ Foxy Adventure/Assets/Script/Dialogue/TypeEffect.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TypeEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float typeSpeed = 50f;
+    public Coroutine Run(string textToType, TMP_Text labelText)
     {
-        
+        return StartCoroutine(TypeText(textToType, labelText));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator TypeText(string textToType, TMP_Text labelText)
     {
-        
+        labelText.text = string.Empty;
+
+        float f = 0;
+        int charIndex = 0;
+
+        while (charIndex < textToType.Length)
+        {
+            f += Time.deltaTime * typeSpeed;
+            charIndex = Mathf.FloorToInt(f);
+            charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
+
+            labelText.text = textToType.Substring(0, charIndex);
+
+            yield return null;
+        } 
+
+        labelText.text = textToType;
     }
 }
