8489c2d6d0df25cee9d8af9d4a60ceab3a968c9c
Alif Rizki Ramdhana <alifrizki1011@gmail.com>
2025-03-02 20:48:38 +0700

    update: Button Responsive

--- Foxy Adventure/Assets/Script/Dialogue Script/ResponseHandler.cs
+++ Foxy Adventure/Assets/Script/Dialogue Script/ResponseHandler.cs
@@ -1,12 +1,21 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class ResponseHandler : MonoBehaviour
 {
     [SerializeField] private RectTransform responseBox;
     [SerializeField] private RectTransform responseButtonTemplate;
     [SerializeField] private RectTransform responseContainer;
+
+    private DialogueUI dialogueUI;
+    private List<GameObject> tempResponseButtons = new List<GameObject>();
+
+    private void Start()
+    {
+        dialogueUI = GetComponent<DialogueUI>();   
+    }
 
     public void ShowResponses(Response[] responses)
     {
@@ -19,6 +28,8 @@ public class ResponseHandler : MonoBehaviour
             responseButton.GetComponent<TMP_Text>().text = response.ResponseText;
             responseButton.GetComponent<Button>().onClick.AddListener(() => OnPickedResponse(response));
 
+            tempResponseButtons.Add(responseButton);
+
             responseBoxWidth += responseButtonTemplate.sizeDelta.x;
         }
 
@@ -28,6 +39,15 @@ public class ResponseHandler : MonoBehaviour
 
     private void OnPickedResponse(Response response)
     {
+        responseBox.gameObject.SetActive(false);
 
+        foreach (GameObject button in tempResponseButtons)
+        {
+            Destroy(button);
+        }
+
+        tempResponseButtons.Clear();
+
+        dialogueUI.ShowDialogue(response.DialogueObject);
     }
 }
