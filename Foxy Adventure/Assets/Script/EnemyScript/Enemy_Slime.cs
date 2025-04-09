4f5c18a701de532935acf77d050084fd24ee7bc7
Alif Rizki Ramdhana <alifrizki1011@gmail.com>
2025-04-09 20:21:24 +0700

    update: CP1_EnemyAI

--- Foxy Adventure/Assets/Script/EnemyScript/Enemy_Slime.cs
+++ Foxy Adventure/Assets/Script/EnemyScript/Enemy_Slime.cs
@@ -3,15 +3,16 @@ using Pathfinding;
 
 public class Enemy_Slime : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+     public AIPath aiPath;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        } else if (aiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
     }
 }
